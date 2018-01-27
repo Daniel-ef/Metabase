2ed8d6265141312c9607f5efcfd0703989a49413
dshaklein <d.shaklein@sailplay.ru>
2018-01-27 22:48:25 +0300

    improvements

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+
 import smtplib
 from email import encoders
 from email.mime.multipart import MIMEMultipart, MIMEBase
@@ -6,11 +7,12 @@ from email.mime.text import MIMEText
 from urllib.parse import quote
 from urllib.request import urlretrieve
 
-import openpyxl
-from openpyxl import load_workbook
+import pandas as pd
+
+from settings import CHILDREN, MESSAGE_TEMPLATE, CURATOR_EMAIL, CURATOR_PASS, START_DATE, END_DATE
 
 
-def download_file(start_date, end_date, id, file_name):
+def download_file(start_date, end_date, user_id, file_name):
 
     if file_name in os.listdir("."):
         print("File exists")
@@ -18,42 +20,38 @@ def download_file(start_date, end_date, id, file_name):
 
     print("Downloading...")
     request_string = 'https://metabase.foxford.ru/public/question/782ef688-5f95-41ba-8cab-c285650cca86.xlsx?parameters='
-    parametrs = '[{"type":"date/single","target":["variable",["template-tag","startDate"]],"value":"%s"},' \
-              '{"type":"date/single","target":["variable",["template-tag","endDate"]],"value":"%s"},' \
-              '{"type":"category","target":["variable",["template-tag","user_id"]],"value":"%s"},' \
-              '{"type":"category","target":["variable",["template-tag","school_year_id"]],"value":"6"}' \
-                     ']' % (start_date, end_date, id)
+    params = """
+        [{"type":"date/single","target":["variable",["template-tag","startDate"]],"value":"%s"},
+        {"type":"date/single","target":["variable",["template-tag","endDate"]],"value":"%s"},
+        {"type":"category","target":["variable",["template-tag","user_id"]],"value":"%s"},
+        {"type":"category","target":["variable",["template-tag","school_year_id"]],"value":"6"}]
+        """ % (start_date, end_date, user_id)
 
-    url = request_string + quote(parametrs)
+    url = request_string + quote(params)
     urlretrieve(url, file_name)
     print("File downloaded")
 
 
 def hide_columns(file_name):
-    columns_to_delete = [1, 2, 4, 5]
+    print('Trying to hide columns')
+    columns_to_delete = [0, 1, 3, 4]
 
-    def hide_column(ws, column_id):
-        if isinstance(column_id, int):
-            column_id = openpyxl.utils.get_column_letter(column_id)
-        column_dimension = ws.column_dimensions[column_id]
-        column_dimension.hidden = True
-
-    wb = load_workbook(file_name)
-    ws = wb.active
-    for col_num in columns_to_delete:
-        hide_column(ws, col_num)
-
-    wb.save(file_name)
-    print("Columns hiden")
+    df = pd.read_excel(file_name)
+    df.drop(df.columns[columns_to_delete], axis=1, inplace=True)
+    writer = pd.ExcelWriter(file_name)
+    df.to_excel(writer, 'Sheet1')
+    # wb = load_workbook(file_name)
+    # ws = wb.active
+    # for col in map(get_col_letter, columns_to_delete):
+    #     ws.column_dimensions[col].hidden = True
+    # wb.save(file_name)
+    print("Columns hidden")
 
 
 def send_email(filename, email):
     with open(filename, "rb") as fp:
         attachment = fp.read()
-    with open("msg_body", encoding="utf-8") as fp:
-        body = MIMEText(fp.read(), "plain")
-    with open("auth", "r") as fp:
-        login, password = fp.read().split()
+    body = MIMEText(MESSAGE_TEMPLATE)
 
     # Compose attachment
     part = MIMEBase('application', "octet-stream")
@@ -64,7 +62,7 @@ def send_email(filename, email):
     # Compose message
     msg = MIMEMultipart()
     msg["Subject"] = "Фоксфорд. Еженедельный отчёт"
-    msg["From"] = login
+    msg["From"] = CURATOR_EMAIL
     msg["To"] = email
 
     msg.attach(part)
@@ -72,9 +70,9 @@ def send_email(filename, email):
 
     # Establish connection
     server = smtplib.SMTP_SSL()
-    server_addres = login.split("@")[1]
+    server_addres = CURATOR_EMAIL.split("@")[1]
     server.connect('smtp.%s' % server_addres)
-    server.login(login, password)
+    server.login(CURATOR_EMAIL, CURATOR_PASS)
 
     # Send mail
     server.sendmail(msg["From"], msg["To"], msg.as_string())
@@ -83,19 +81,17 @@ def send_email(filename, email):
 
 
 if __name__ == "__main__":
-    print("Put dates like: 2017-01-18")
-    start_date, end_date = input("Start date: "), input("End date: ")
+    print("Put dates like: 2018-01-18")
 
-    with open("email-id", "r") as fp:
-        e_id = list(map(lambda x: x.split(":"), fp.read().split()))
+    for child in CHILDREN:
+        file_name = "{}-{}-{}.xlsx".format(
+            START_DATE.replace('-', ''),
+            END_DATE.replace('-', ''),
+            child['name']
+        )
 
-    for email, child_id in e_id:
-        file_name = "{}---{}---{}.xlsx".format(start_date, end_date, child_id)
-
-        download_file(start_date, end_date, child_id, file_name)
+        download_file(START_DATE, END_DATE, child['user_id'], file_name)
 
         hide_columns(file_name)
-
-        send_email(file_name, email)
-
+        send_email(file_name, child['parent_email'])
 
